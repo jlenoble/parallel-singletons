aa65e01b6daa318cad37d45c5dd7a6d0e6d024d5
Jason Lenoble <jason.lenoble@gmail.com>
2016-11-16 14:32:01 +0100

    Test with 3d vectors

--- test/parallel-singletons.test.js
+++ test/parallel-singletons.test.js
@@ -1,6 +1,5 @@
 import Muter, {muted} from 'muter';
-import OneGo from 'one-go';
-import {SingletonFactory} from 'singletons';
+import {ogArgs} from 'one-go';
 import {expect} from 'chai';
 import {ParallelSingletonFactory} from '../src/parallel-singletons';
 
@@ -11,7 +10,7 @@ describe('Testing ParallelSingletons', function() {
     [console, 'error']
   );
 
-  it(`ParallelSingletonFactory says 'Hello!'`, muted(muter, function() {
+  it(`Testing with loggers`, muted(muter, function() {
     class Class {
       constructor(logger, method) {
         this.logger = logger;
@@ -33,6 +32,65 @@ describe('Testing ParallelSingletons', function() {
 
     s.log('Hello!');
     expect(muter.getLogs()).to.equal('Hello!\nHello!\n');
+  }));
+
+  it(`Testing with 3d-vectors`, muted(muter, function() {
+    class Vector {
+      constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+      }
+
+      plus(vec) {
+        this.x += vec.x;
+        this.y += vec.y;
+        this.z += vec.z;
+      }
+
+      times(num) {
+        this.x *= num;
+        this.y *= num;
+        this.z *= num;
+      }
+
+      product(vec) {
+        return this.x * vec.x + this.y * vec.y + this.z * vec.z;
+      }
+
+      module() {
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+      }
+    }
+
+    const Singleton = ParallelSingletonFactory(Vector,
+      ['literal', 'literal', 'literal']);
+
+    const s = Singleton([
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 1]
+    ]);
+
+    s.times(6);
+    s.plus({x: 1, y: 1, z: 1});
+    expect(s.product({x: 1, y: 1, z: 1})).to.eql([9, 9, 9]);
+    s.plus({x: -1, y: -1, z: -1});
+    expect(s.module()).to.eql([6, 6, 6]);
+    s.plus(ogArgs(
+      new Vector(-6, 0, 0),
+      new Vector(0, -6, 0),
+      new Vector(0, 0, -6)
+    ));
+    expect(s.module()).to.eql([0, 0, 0]);
+
+    const t = Singleton([
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 1]
+    ]);
+    expect(t.module()).to.eql([0, 0, 0]); // t **IS** s, which has changed
+    expect(s).to.equal(t);
   }));
 
 });
